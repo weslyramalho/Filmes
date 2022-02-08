37ab52c9091d7d18a6a2e07bb90d6b76f237fc16
Wesly <weslyramalho@yahoo.com.br>
2022-02-08 14:40:58 -0300

    Criado repositories

---
+++ src/main/java/com/filmes/filmes/repositories/FilmesRepository.java
@@ -0,0 +1,45 @@
+package com.filmes.filmes.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Repository;
+
+import com.filmes.filmes.entities.Filmes;
+
+@Repository
+public class FilmesRepository {
+	private  final List<Filmes> filmesList;
+	
+	public FilmesRepository() {
+		this.filmesList = new ArrayList<>();
+	}
+	
+	public void cadastrarFilme(Filmes filmes) {
+		this.filmesList.add(filmes);
+		
+	}
+	
+	public int cont() {
+		return filmesList.size();
+	}
+	
+	public void remove(Integer id) {
+		filmesList.removeIf(i -> i.getId() == id);
+	}
+	
+	public List<Filmes> buscarFilmesPorNome(final String filmes){
+		return filmesList.stream()
+				.filter(f -> f.getNome().equalsIgnoreCase(filmes))
+				.collect(Collectors.toList());
+	}
+	public List<Filmes> buscarFilmesPorDiretor(final String diretor){
+		return filmesList.stream()
+				.filter(f -> f.getDiretor().equalsIgnoreCase(diretor))
+				.collect(Collectors.toList());
+	}
+	
+	
+
+}
