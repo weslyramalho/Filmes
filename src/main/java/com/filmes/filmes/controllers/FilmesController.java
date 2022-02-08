8aa274b3cc6bb7824e18f54119624baa4456701f
Wesly <weslyramalho@yahoo.com.br>
2022-02-08 16:15:26 -0300

    Post em filmesController implementado

--- src/main/java/com/filmes/filmes/controllers/FilmesController.java
+++ src/main/java/com/filmes/filmes/controllers/FilmesController.java
@@ -1,5 +1,6 @@
 package com.filmes.filmes.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,16 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filmes.filmes.entities.Filmes;
+import com.filmes.filmes.repositories.FilmesRepository;
 import com.filmes.filmes.services.FilmesServices;
 
 @RestController
 @RequestMapping("/filmes")
 public class FilmesController {
-	FilmesServices filmesServices = new FilmesServices();
+	
+	@Autowired
+	FilmesServices filmesServices;
+	
+	@Autowired
+	FilmesRepository filmesRepository;
 	
 	@PostMapping
-	public ResponseEntity<Object> post(@RequestBody Filmes filmes){
-		filmesServices.addNovoFilme(filmes);
+	public ResponseEntity<Integer> post(@RequestBody final Filmes filmes){
+		if(filmesRepository.findAll().stream().anyMatch(f -> f.getNome().equalsIgnoreCase(filmes.getNome())) &&
+				filmesRepository.findAll().stream().anyMatch(f -> f.getDiretor().equalsIgnoreCase(filmes.getDiretor())))){
+					throw new ResponseStatusException(HttpStatus.CONFLICT, "Ops. Este filme já esta cadastrado!");
+				}
+		filmesServices.cadastrarFilme(filmes);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 
 	}
