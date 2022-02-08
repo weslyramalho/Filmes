2da2b3f5b01c371c657140a90d673a859e84a472
Wesly <weslyramalho@yahoo.com.br>
2022-02-08 16:31:35 -0300

    update implementado em filmesController

--- src/main/java/com/filmes/filmes/controllers/FilmesController.java
+++ src/main/java/com/filmes/filmes/controllers/FilmesController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.filmes.filmes.entities.Filmes;
 import com.filmes.filmes.repositories.FilmesRepository;
@@ -30,7 +32,7 @@ public class FilmesController {
 	@PostMapping
 	public ResponseEntity<Integer> post(@RequestBody final Filmes filmes){
 		if(filmesRepository.findAll().stream().anyMatch(f -> f.getNome().equalsIgnoreCase(filmes.getNome())) &&
-				filmesRepository.findAll().stream().anyMatch(f -> f.getDiretor().equalsIgnoreCase(filmes.getDiretor())))){
+				filmesRepository.findAll().stream().anyMatch(f -> f.getDiretor().equalsIgnoreCase(filmes.getDiretor()))){
 					throw new ResponseStatusException(HttpStatus.CONFLICT, "Ops. Este filme já esta cadastrado!");
 				}
 		filmesServices.cadastrarFilme(filmes);
@@ -44,8 +46,18 @@ public class FilmesController {
 	}
 	
 	 @GetMapping("/{id}")
-	    public Filmes buscarPorId(@PathVariable("id") Integer id){
+	 public Filmes buscarPorId(@PathVariable("id") Integer id){
 	        return filmesServices.buscaPorId(id);
+	    }
+	 
+	  @PatchMapping
+	  public ResponseEntity<Object> update(@RequestBody final Filmes filmes){
+	        if(filmes.getNota() < 1 || filmes.getNota() > 5){
+	            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Ops.Esta nota não é valida, favor inserir um valor entre 1-5");
+	        }
+	        filmesServices.update(filmes);
+	        final String up = "Filme alterado com sucesso";
+	        return new ResponseEntity<>(up,HttpStatus.NO_CONTENT);
 	    }
 
 }
