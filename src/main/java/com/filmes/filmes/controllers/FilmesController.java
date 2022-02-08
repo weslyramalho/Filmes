583d09ddb23ba3bfc5879ad2bf827c6478203a58
Wesly <weslyramalho@yahoo.com.br>
2022-02-08 16:22:45 -0300

    buscaPorId implementado em filmesController

--- src/main/java/com/filmes/filmes/controllers/FilmesController.java
+++ src/main/java/com/filmes/filmes/controllers/FilmesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,10 @@ public class FilmesController {
 	public List<Filmes> findAll(@RequestParam(required = false)String filmes, String diretor){
 		return filmesServices.findAll(filmes, diretor);
 	}
+	
+	 @GetMapping("/{id}")
+	    public Filmes buscarPorId(@PathVariable("id") Integer id){
+	        return filmesServices.buscaPorId(id);
+	    }
 
 }
