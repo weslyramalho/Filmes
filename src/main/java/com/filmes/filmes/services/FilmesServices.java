8fe734e5ee1f4d3e1728b4bb90178a7fec9ce789
Wesly <weslyramalho@yahoo.com.br>
2022-02-08 15:38:11 -0300

    Metodo cadastrarFilme implementado em filmes services

--- src/main/java/com/filmes/filmes/services/FilmesServices.java
+++ src/main/java/com/filmes/filmes/services/FilmesServices.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.filmes.filmes.entities.Filmes;
 import com.filmes.filmes.repositories.FilmesRepository;
@@ -28,7 +30,18 @@ public class FilmesServices {
 		return filmesRepository.buscaPorId(id);
 	}
 	
-	public Filmes cadastrarFilme(Filmes filmes);
+	public Filmes cadastrarFilme(final Filmes filmes) {
+		
+		if(filmes.getNota() < 1 || filmes.getNota() > 5) {
+			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Favor inserir um valor entre 1 - 5");
+		}
+		else if(filmes.getId() == null) {
+			filmes.setId((filmesRepository.cont() + 1));
+		}
+		
+		filmesRepository.cadastrarFilme(filmes);
+		return filmes;
+	}
 	public void remove(Integer id);
 	public void update(Filmes filmes);
 
