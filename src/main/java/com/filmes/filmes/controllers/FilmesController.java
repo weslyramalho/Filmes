9a51fe92b844bec6c87b860d0c511c7fe4c052f3
Wesly <weslyramalho@yahoo.com.br>
2022-02-08 16:19:49 -0300

    findAll implementado em filmesController

--- src/main/java/com/filmes/filmes/controllers/FilmesController.java
+++ src/main/java/com/filmes/filmes/controllers/FilmesController.java
@@ -1,11 +1,15 @@
 package com.filmes.filmes.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filmes.filmes.entities.Filmes;
@@ -31,6 +35,11 @@ public class FilmesController {
 		filmesServices.cadastrarFilme(filmes);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 
+	}
+	
+	@GetMapping
+	public List<Filmes> findAll(@RequestParam(required = false)String filmes, String diretor){
+		return filmesServices.findAll(filmes, diretor);
 	}
 
 }
